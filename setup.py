4dcfd0807298bb2066b2d196f837792b25a5d322
Daria <sigulia39@gmail.com>
2024-09-08 13:57:16 +0300

    add find_packages function

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 REQUIRES = [
     'requests',
     'allure-pytest',
     'pydantic',
-    'restclient',
-    'typing'
+    'restclient'
 ]
 
 setup(
     name='dm_api_account',
     version='0.0.1',
-    packages=['dm_api_account'],
+    packages=find_packages(),
+    include_package_data=True,
     url='https://github.com/DoraSigulia/dm_api_account.git',
     license='MIT',
     author='Daria Sigulya',
