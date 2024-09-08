558ee09b29f153e230f784820f9d7a0a05d99ba3
Daria <sigulia39@gmail.com>
2024-09-08 14:06:56 +0300

    fix init

--- dm_api_account/apis/__init__.py
+++ dm_api_account/apis/__init__.py
@@ -1,2 +1,2 @@
-from dm_api_account.dm_api_account.apis.account_api import AccountApi
-from dm_api_account.dm_api_account.apis.login_api import LoginApi
+from dm_api_account.apis.account_api import AccountApi
+from dm_api_account.apis.login_api import LoginApi
